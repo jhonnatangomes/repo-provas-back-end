b7fec82c521ce6047ea159e87f21f3bf34f1c625
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-12-17 16:02:24 -0300

    corrected connection file

--- src/database.ts
+++ src/database.ts
@@ -13,9 +13,7 @@ export default async function connect() {
         name: 'default',
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        entities: [
-            `${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.ts`,
-        ],
+        entities: ['src/entities/*.ts', 'dist/src/entities/*.js'],
         ssl: process.env.NODE_ENV === 'production',
     });
     await connection.connect();
